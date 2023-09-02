200982f529b7a8018742baeffebea79fb8f6ed9d
Fadi Ashraf <98248248+Faaadi10@users.noreply.github.com>
2023-09-02 23:24:58 +0530

    Create item-locator.py

---
+++ item-locator.py
@@ -0,0 +1,13 @@
+print("Item Locator")
+row1 = ["⬜", "⬜", "⬜"]
+row2 = ["⬜", "⬜", "⬜"]
+row3 = ["⬜", "⬜", "⬜"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("where do you want to put the item? First type the column then type the row")
+horizontal = int(position[0])
+vertical = int (position[1])
+
+selected_row = map[vertical -1]
+selected_row[horizontal - 1] = 'X'
+print(f"{row1}\n{row2}\n{row3}")
