ab80d3a30cdf3d7e26ab1a203e399f46229b7a5a
Fadi Ashraf <98248248+Faaadi10@users.noreply.github.com>
2023-09-02 22:40:49 +0530

    Create tab_splitter.py
    Small program using Random module.

---
+++ tab_splitter.py
@@ -0,0 +1,9 @@
+import random
+
+print("Tab Splitter")
+names_string = input("Give me the names of everyone, separated by a comma\n")
+names = names_string.split(",")
+number = len(names)
+random = random.randint(1, number - 1)
+print(random)
+print(names[random] + " will have to pay the bill")
