653b0c5e4f680ee2bc478284c8f1cd9b08865c2a
Fadi Ashraf <98248248+Faaadi10@users.noreply.github.com>
2023-09-06 22:20:17 +0530

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -22,3 +22,30 @@ for char in range(1, number_letters + 1):
     password = password + random.choice(symbols)
 
 print("Your Password is : " + password)
+
+
+
+#hard-level (mixing up the order of letters, symbols and numbers)
+
+
+password_list = []
+
+for char in range(1, number_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, number_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+for char in range(1, number_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+print(password_list)
+
+#to convert list into string
+
+password = ""
+for char in password_list:
+     password += char
+
+print(f"your password is : {password}")
