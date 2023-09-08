72c99397b0458dc13d2247107d83d1cb7e8dc748
Fadi Ashraf <98248248+Faaadi10@users.noreply.github.com>
2023-09-08 10:04:52 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,27 +1,118 @@
 # Randomly chose a word between a list of words.
+# Randomly chose a word between a list of words.
 import random
 last_names = ["Messi", "Ronaldo", "Mbappe", "Salah", "Hazard", "Lewandowski", "Modric", "Bale", "Suarez", "Kroos"]
 chosen_word = random.choice(last_names)
 print("Choosen word is " + chosen_word)
 
+# Setting the total number of lives of hangman to be 6
+lives = 6
 
-# creating a list
+# Setting the hangman in a string. Each position of the string denotes each life losing.
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+# Adding blanks at each letter position
 display = []
-word_length = len(chosen_word)
-for letter in chosen_word:
+length = len(chosen_word)
+for char in range(length):
     display.append("_")
 print(display)
 
+end_of_game = False
 
-# to check if guessed letter is present in the random chosen word
-# using a while loop to let the user guess again. The loop should stop only when user has guessed all the letters in the chosen word and display has noo more blanks left.
 
-while
-    guess = input("Guess a letter: ").lower()
-    for position in range(word_length):
+while not end_of_game:
+
+    # Asking user to guess a letter from the word
+    guess = input("Guess a letter : ").lower()
+
+
+
+
+    # To check if the guess is in the word
+    for letter in chosen_word:
+        if letter == guess:
+            print("Right")
+        else:
+            print("Wrong")
+
+
+    # Replacing the blanks with correct guess letter
+    for position in range(length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
     print(display)
 
-# using a while loop to let the user guess again. The loop should stop only when user has guessed all the letters in the chosen word and display has noo more blanks left.
+    if guess not in chosen_word:
+        lives = lives - 1
+        if lives == 0:
+            end_of_game = True
+            print("You Lost")
+
+
+
+
+    # Checking won or not
+
+    if "_" not in display:
+        end_of_game = True
+        print("You Win")
+
+
+    print(stages[lives])
